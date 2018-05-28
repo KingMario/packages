252e098917314eda17c8a44d31c97d5d3301de51
Changyu Geng <cgeng@vmware.com>
2018-05-28 12:03:28 +0800

    improve unit test case

--- projects/ngx-trim-directive/src/test/ngx-trim.directive.spec.ts
+++ projects/ngx-trim-directive/src/test/ngx-trim.directive.spec.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import {
   async,
   ComponentFixture,
   TestBed,
 } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { NgxTrimDirective } from '../lib/ngx-trim.directive';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   template: `
-    <input type="text" [value]="value" trim>
+    <input type="text" #input1 [(ngModel)]="value" trim>
+    <div [formGroup]="formA">
+      <input type="text" #input2 formControlName="fieldA" trim="blur">
+    </div>
   `,
 })
 class TestComponent {
+  @ViewChild('input1') input1: ElementRef;
+  @ViewChild('input2') input2: ElementRef;
+
+  readonly fieldA = new FormControl('');
+  readonly formA = new FormGroup({
+    fieldA: this.fieldA,
+  });
+
   value = '';
 }
 
@@ -23,6 +43,10 @@ describe('NgxTrimDirective', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+      ],
       declarations: [
         NgxTrimDirective,
         TestComponent,
@@ -37,20 +61,41 @@ describe('NgxTrimDirective', () => {
     fixture.detectChanges();
   });
 
-  it('should trim the input value', () => {
-    const nativeEl = fixture.debugElement.query(By.directive(NgxTrimDirective)).nativeElement;
+  it('should trim the value of input1 on input or blur', () => {
+    const el1 = component.input1.nativeElement;
 
-    nativeEl.value = 'ngxTrimDirective   ';
-    nativeEl.dispatchEvent(new Event('input'));
-    expect(nativeEl.value).toBe('ngxTrimDirective');
+    el1.value = 'ngxTrimDirective   ';
+    el1.dispatchEvent(new Event('input'));
+    expect(el1.value).toBe('ngxTrimDirective');
+    expect(component.value).toBe('ngxTrimDirective');
 
-    nativeEl.value = '   ngxTrimDirective';
-    nativeEl.dispatchEvent(new Event('input'));
-    expect(nativeEl.value).toBe('ngxTrimDirective');
+    el1.value = '   ngxTrimDirective';
+    el1.dispatchEvent(new Event('input'));
+    expect(el1.value).toBe('ngxTrimDirective');
+    expect(component.value).toBe('ngxTrimDirective');
 
-    nativeEl.value = '   ngxTrimDirective   ';
-    nativeEl.dispatchEvent(new Event('blur'));
-    expect(nativeEl.value).toBe('ngxTrimDirective');
+    el1.value = '   ngxTrimDirective   ';
+    el1.dispatchEvent(new Event('blur'));
+    expect(el1.value).toBe('ngxTrimDirective');
+    expect(component.value).toBe('ngxTrimDirective');
+  });
+
+  it('should not trim the value of input2 on input', () => {
+    const el2 = component.input2.nativeElement;
+
+    el2.value = 'ngxTrimDirective   ';
+    el2.dispatchEvent(new Event('input'));
+    expect(el2.value).toBe('ngxTrimDirective   ');
+    expect(component.fieldA.value).toBe('ngxTrimDirective   ');
+  });
+
+  it('should trim the value of input2 on blur', () => {
+    const el2 = component.input2.nativeElement;
+
+    el2.value = 'ngxTrimDirective   ';
+    el2.dispatchEvent(new Event('blur'));
+    expect(el2.value).toBe('ngxTrimDirective');
+    expect(component.fieldA.value).toBe('ngxTrimDirective');
   });
 
 });
